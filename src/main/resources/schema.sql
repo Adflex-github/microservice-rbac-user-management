90f8fd2c40da08e34b82770d84e0b8ae0cb29435
subhashm <p03ofConcur>
2021-06-06 22:04:40 +0100

    Change to PostgresDb

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,5 @@
+-- SQLINES DEMO *** sktop version to convert large SQL scripts,
+-- SQLINES DEMO *** ny issues with Online conversion.
 DROP TABLE IF EXISTS contacts;
 DROP TABLE IF EXISTS addresses;
 
@@ -8,54 +10,64 @@ DROP TABLE IF EXISTS permissions;
 DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS users;
 
-CREATE TABLE `roles` (
-  `id` BIGINT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `role` varchar(80) NOT NULL UNIQUE
+DROP SEQUENCE IF EXISTS roles_seq;
+DROP SEQUENCE IF EXISTS permissions_seq;
+DROP SEQUENCE IF EXISTS users_seq;
+
+CREATE SEQUENCE roles_seq;
+
+CREATE TABLE roles (
+  id BIGINT NOT NULL DEFAULT NEXTVAL ('roles_seq') PRIMARY KEY,
+  role varchar(80) NOT NULL UNIQUE
 );
 
-CREATE TABLE `permissions` (
-  `id` BIGINT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `permission` varchar(80) NOT NULL UNIQUE,
-  enabled TINYINT DEFAULT '1',
+CREATE SEQUENCE permissions_seq;
+
+CREATE TABLE permissions (
+  id BIGINT NOT NULL DEFAULT NEXTVAL ('permissions_seq') PRIMARY KEY,
+  permission varchar(80) NOT NULL UNIQUE,
+  enabled BOOLEAN DEFAULT true,
   note varchar(255) DEFAULT NULL
 );
 
 CREATE TABLE permissions_roles (
-  `permission_id` BIGINT(20) NOT NULL,
-  `role_id` BIGINT(20) NOT NULL,
+  permission_id BIGINT NOT NULL,
+  role_id BIGINT NOT NULL,
   PRIMARY KEY (permission_id, role_id),
   FOREIGN KEY (permission_id) REFERENCES permissions(id),
   FOREIGN KEY (role_id) REFERENCES roles(id)
 );
 
-CREATE TABLE `users` (
-  `id` BIGINT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `username` varchar(100) NOT NULL UNIQUE,
-  `password` varchar(255) NOT NULL,
-  `name` varchar(100) DEFAULT NULL,
-  `surname` varchar(100) DEFAULT NULL,
-  gender TINYINT DEFAULT NULL,
+CREATE SEQUENCE users_seq;
+
+CREATE TABLE users (
+  id BIGINT NOT NULL DEFAULT NEXTVAL ('users_seq') PRIMARY KEY,
+  username varchar(100) NOT NULL UNIQUE,
+  password varchar(255) NOT NULL,
+  name varchar(100) DEFAULT NULL,
+  surname varchar(100) DEFAULT NULL,
+  gender SMALLINT DEFAULT NULL,
   birth_date DATE DEFAULT NULL,
-  enabled TINYINT DEFAULT '1',
-  creation_dt timestamp NOT NULL DEFAULT current_timestamp,
-  updated_dt timestamp DEFAULT current_timestamp,
-  login_dt timestamp NULL,
+  enabled BOOLEAN DEFAULT true,
+  creation_dt timestamp(0) NOT NULL DEFAULT current_timestamp,
+  updated_dt timestamp(0) DEFAULT current_timestamp,
+  login_dt timestamp(0) NULL,
   note varchar(255) DEFAULT NULL,
-  secured TINYINT DEFAULT '0'
+  secured BOOLEAN DEFAULT false
 );
 
 CREATE TABLE users_roles (
-  `user_id` BIGINT(20) NOT NULL,
-  `role_id` BIGINT(20) NOT NULL,
+  user_id BIGINT NOT NULL,
+  role_id BIGINT NOT NULL,
   PRIMARY KEY (user_id, role_id),
   FOREIGN KEY (user_id) REFERENCES users(id),
   FOREIGN KEY (role_id) REFERENCES roles(id)
 );
 
-CREATE TABLE `contacts` (
-  user_id BIGINT(20) NOT NULL PRIMARY KEY,
-  `email` varchar(255) NOT NULL UNIQUE,
-  `phone` varchar(20) DEFAULT NULL,
+CREATE TABLE contacts (
+  user_id BIGINT NOT NULL PRIMARY KEY,
+  email varchar(255) NOT NULL UNIQUE,
+  phone varchar(20) DEFAULT NULL,
   skype varchar(255) DEFAULT NULL,
   facebook varchar(255) DEFAULT NULL,
   linkedin varchar(255) DEFAULT NULL,
@@ -64,13 +76,13 @@ CREATE TABLE `contacts` (
   FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
-CREATE TABLE `addresses` (
-  user_id BIGINT(20) NOT NULL PRIMARY KEY,
-  `address` varchar(255) DEFAULT NULL,
-  `address2` varchar(255) DEFAULT NULL,
-  `city` varchar(20) DEFAULT NULL,
-  `country` varchar(20) DEFAULT NULL,
-  `zip_code` varchar(20) DEFAULT NULL,
+CREATE TABLE addresses (
+  user_id BIGINT NOT NULL PRIMARY KEY,
+  address varchar(255) DEFAULT NULL,
+  address2 varchar(255) DEFAULT NULL,
+  city varchar(20) DEFAULT NULL,
+  country varchar(20) DEFAULT NULL,
+  zip_code varchar(20) DEFAULT NULL,
   FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
